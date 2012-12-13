34370707355897904a0af21515a68bba4e26a06c
Nick Craig-Wood <nick@craig-wood.com>
2012-12-13 21:18:42 +0000

    Initial IPv6 support

--- nflog.go
+++ nflog.go
@@ -45,20 +45,32 @@ import "C"
 
 const (
 	MAX_CAPLEN = 4096
-	McastGroup = 1
+	McastGroupIPv4 = 4
+	McastGroupIPv6 = 6
 
-	// 20 bytes IP Header - http://en.wikipedia.org/wiki/IPv4
+	// Shared
+	IpVersion      = 0
+	IpVersionShift = 4
+	IpVersionMask  = 0x0f
+
+	// 20 bytes IPv4 Header - http://en.wikipedia.org/wiki/IPv4
 	Ip4LengthOffest = 2
 	Ip4SrcOffset    = 12
 	Ip4DstOffset    = 16
-	IpHeaderSize    = 20
+	Ip4HeaderSize   = 20
+
+	// 40 bytes IPv6 Header - http://en.wikipedia.org/wiki/IPv6_packet
+	Ip6LengthOffest = 4
+	Ip6SrcOffset    = 8
+	Ip6DstOffset    = 24
+	Ip6HeaderSize   = 40
 )
 
 // NfLog
 type NfLog struct {
 	h *C.struct_nflog_handle
 	fd int
-	qh *C.struct_nflog_g_handle
+	qh4, qh6 *C.struct_nflog_g_handle
 }
 
 // Current nflog error
@@ -70,28 +82,39 @@ func nflog_error() error {
 func goCallback(cprefix *C.char, payload_len C.int, payload unsafe.Pointer) {
 	prefix := C.GoString(cprefix)
 	packet := C.GoBytes(payload, payload_len)
-	log.Printf("%s: size %d", prefix, payload_len)
-	// Decode the IPv4 packet parts we need
-	ipv4 := packet		// FIXME IP6?
-	src := ipv4[Ip4SrcOffset : Ip4SrcOffset+4]
-	dst := ipv4[Ip4DstOffset : Ip4DstOffset+4]
-	length := int(ipv4[Ip4LengthOffest])<<8 + int(ipv4[Ip4LengthOffest+1])
-	log.Printf("IPv4 message From %s To %s Size %d", net.IP(src), net.IP(dst), length)
+	// Peek the IP Version out of the header
+	ip_version := packet[IpVersion] >> IpVersionShift & IpVersionMask
+	log.Printf("Received %s: size %d, IPv%d", prefix, payload_len, ip_version)
+	var src, dst net.IP
+	var length int
+	switch ip_version {
+	case 4:
+		if len(packet) < Ip4HeaderSize {
+			log.Printf("Short IPv4 packet %d bytes", len(packet))
+			return
+		}
+		src = net.IP(packet[Ip4SrcOffset : Ip4SrcOffset+4])
+		dst = net.IP(packet[Ip4DstOffset : Ip4DstOffset+4])
+		length = int(packet[Ip4LengthOffest])<<8 + int(packet[Ip4LengthOffest+1])
+	case 6:
+		if len(packet) < Ip6HeaderSize {
+			log.Printf("Short IPv6 packet %d bytes", len(packet))
+			return
+		}
+		src = net.IP(packet[Ip6SrcOffset : Ip6SrcOffset+16])
+		dst = net.IP(packet[Ip6DstOffset : Ip6DstOffset+16])
+		length = int(packet[Ip6LengthOffest])<<8 + int(packet[Ip6LengthOffest+1])
+	default:
+		log.Printf("Bad IP version: %d", ip_version)
+		return
+	}
+	log.Printf("IPv%d message From %s To %s Size %d", ip_version, src, dst, length)
 }
 
-// Open the nflog
-func NewNfLog() *NfLog {
-	h := C.nflog_open()
-	if h == nil {
-		log.Fatal("Failed to open NFLOG: %s", nflog_error())
-	}
-	log.Println("Binding nfnetlink_log to AF_INET")
-	if C.nflog_bind_pf(h, C.AF_INET) < 0 {
-		log.Fatal("nflog_bind_pf failed: %s", nflog_error())
-	}
-
-	log.Printf("Binding this socket to group %d", McastGroup)
-	qh := C.nflog_bind_group(h, McastGroup)
+// Connects to the group specified with the size
+func (nflog *NfLog) makeGroup(group, size uint) *C.struct_nflog_g_handle {
+	log.Printf("Binding this socket to group %d", group)
+	qh := C.nflog_bind_group(nflog.h, (C.u_int16_t)(group))
 	if qh == nil {
 		log.Fatal("nflog_bind_group failed: %s", nflog_error())
 	}
@@ -106,17 +129,32 @@ func NewNfLog() *NfLog {
 		log.Fatal("nflog_set_qthresh failed: %s", nflog_error())
 	}
 
-	log.Println("Setting copy_packet mode")
-	// FIXME header size for IPv6
-	if C.nflog_set_mode(qh, C.NFULNL_COPY_PACKET, 20) < 0 {
+	log.Printf("Setting copy_packet mode to %d bytes", size)
+	if C.nflog_set_mode(qh, C.NFULNL_COPY_PACKET, (C.uint)(size)) < 0 {
 		log.Fatal("nflog_set_mode failed: %s", nflog_error())
 	}
 
-	return &NfLog{
+	return qh
+}
+
+// Open the nflog
+func NewNfLog() *NfLog {
+	h := C.nflog_open()
+	if h == nil {
+		log.Fatal("Failed to open NFLOG: %s", nflog_error())
+	}
+	log.Println("Binding nfnetlink_log to AF_INET")
+	if C.nflog_bind_pf(h, C.AF_INET) < 0 {
+		log.Fatal("nflog_bind_pf failed: %s", nflog_error())
+	}
+
+	nflog := &NfLog{
 		h: h,
 		fd: int(C.nflog_fd(h)),
-		qh: qh,
 	}
+	nflog.qh4 = nflog.makeGroup(McastGroupIPv4, Ip4HeaderSize)
+	nflog.qh6 = nflog.makeGroup(McastGroupIPv6, Ip6HeaderSize)
+	return nflog
 }
 
 // Receive packets in a loop forever
@@ -135,8 +173,10 @@ func (nflog *NfLog) Loop() {
 
 // Close the NfLog down
 func (nflog *NfLog) Close() {
-	log.Printf("Unbinding this socket from group %d", McastGroup)
-	C.nflog_unbind_group(nflog.qh)
+	log.Printf("Unbinding this socket from group %d", McastGroupIPv4)
+	C.nflog_unbind_group(nflog.qh4)
+	log.Printf("Unbinding this socket from group %d", McastGroupIPv6)
+	C.nflog_unbind_group(nflog.qh6)
 	log.Printf("Closing NFLOG")
 	C.nflog_close(nflog.h)
 }
